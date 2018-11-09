6e029be629c5f92757a23071e8884702298a2332
meishe <meishe_sdk@cdv.com>
2018-11-09 15:32:06 +0800

    aa

---
+++ app/src/main/java/com/maventest/maveantest/MainActivity.java
@@ -0,0 +1,14 @@
+package com.maventest.maveantest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private final String TAG = getClass().getName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
